fcce615796fd2b89364312c4133e984a0b02330d
Ariel <asistercondoriarielleonel@gmail.com>
2024-11-10 02:18:28 +0100

    1mer commit

---
+++ tarea6/tarea6.php
@@ -0,0 +1,21 @@
+<h1>Tarea 6</h1>
+<?php
+$equipos = array(
+    array("La Paz", "Bolìvar", 20000),
+    array("Oruro", "San José", 5000),
+    array("Santa Cruz", "Oriente", 25000)
+);
+
+echo "<table border='1' cellspacing='0'>";
+echo "<caption>Equipos</caption>";
+
+for ($i = 0; $i < count($equipos); $i++) {
+    echo "<tr>";
+    for ($j = 0; $j < count($equipos[$i]); $j++) {
+        echo "<td>" . $equipos[$i][$j] . "</td>";
+    }
+    echo "</tr>";
+}
+
+echo "</table>";
+?>
